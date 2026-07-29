89dbae7aa202590a5747166d04d21ecf45e5026b
ej-a-ej <eja@gmx.fr>
2026-07-29 13:40:27 +0200

    Create Lya.java for personal assistant output

---
+++ code/chapitre1/Lya.java
@@ -0,0 +1,8 @@
+public class Lya {
+    public static void main(String[] args) {
+        System.out.println("=== LYA ASSISTANT ===");
+        System.out.println("Hello! I'm Lya, your personal assistant.");
+        System.out.println("I'm here to help you.");
+        System.out.println("Let's learn together.");
+    }
+}
