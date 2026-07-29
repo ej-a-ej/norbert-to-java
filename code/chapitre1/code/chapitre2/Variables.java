e37717f01105c6fda2913654fe36c18905bb6f05
ej-a-ej <eja@gmx.fr>
2026-07-29 13:41:18 +0200

    Add Variables class with main method

---
+++ code/chapitre1/code/chapitre2/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Lya";
+        double version = 1.0;
+        boolean isReady = true;
+        int messagesProcessed = 0;
+        
+        System.out.println("=== " + name + " ASSISTANT ===");
+        System.out.println("Version: " + version);
+        System.out.println("Ready to help: " + isReady);
+        System.out.println("Messages processed: " + messagesProcessed);
+        System.out.println("Hello! I'm " + name + ", your personal assistant.");
+    }
+}
