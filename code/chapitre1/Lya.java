f244fc80d8bcd961f2bde15b965265450b057d97
ej-a-ej <eja@gmx.fr>
2026-08-03 10:29:05 +0200

    Delete code directory

--- code/chapitre1/Lya.java
+++
@@ -1,8 +0,0 @@
-public class Lya {
-    public static void main(String[] args) {
-        System.out.println("=== LYA ASSISTANT ===");
-        System.out.println("Hello! I'm Lya, your personal assistant.");
-        System.out.println("I'm here to help you.");
-        System.out.println("Let's learn together.");
-    }
-}
